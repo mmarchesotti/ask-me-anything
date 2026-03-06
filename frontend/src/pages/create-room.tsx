78e5f4f80ebc434417944f75ddfc82a84197f9f3
Matheus Marchesotti <mmarferraz@gmail.com>
2026-03-06 12:24:14 -0300

    Finish create room page structure

--- frontend/src/pages/create-room.tsx
+++ frontend/src/pages/create-room.tsx
@@ -12,7 +12,9 @@ export function CreateRoom() {
 					Crie uma sala pública de AMA (Ask me anything) e priorize as perguntas mais importantes para a comunidade.
 				</p>
 
-				<form className='flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:'>
+				<form
+					className='flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1'
+				>
 					<input
 						type='text'
 						name='theme'
@@ -21,7 +23,10 @@ export function CreateRoom() {
 						className='flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500'
 					/>
 
-					<button type='submit' className='bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm'>
+					<button
+						type='submit'
+						className='bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500'
+					>
 						Criar sala
 						<ArrowRight className='size-4' />
 					</button>
